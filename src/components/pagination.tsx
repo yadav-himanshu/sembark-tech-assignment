c6698b770e816816400a38dbe4fecbd8e4ffc178
Himanshu Yadav <himanshuyadav7852@gmail.com>
2026-05-23 11:41:00 +0530

    implement load more pagination

---
+++ src/components/pagination.tsx
@@ -0,0 +1,17 @@
+interface LoadMoreButtonProps{
+    onclick:()=>void;
+}
+const Pagination=({
+    onclick,
+}:LoadMoreButtonProps)=>{
+    return(
+        <div className="flex justify-center mt-6">
+            <button
+            onClick={onclick}
+             className="border px-6 py-3 rounded-lg ">
+                Load More
+            </button>
+        </div>
+    )
+}
+export default Pagination;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import { getAllProducts } from "../services/productService";
 import type { Product } from "../types/product";
 import ProductCard from "../components/ProductCard";
+import Pagination from "../components/pagination";
 
 function HomePage() {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const Items_Per_Click = 8;
+  const [visibleCount, setVisibleCount] =useState(Items_Per_Click);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -15,8 +18,8 @@ function HomePage() {
         const data = await getAllProducts();
         // console.log(data);
         // console.log(data[0]);
-        // setProducts(data);
-        setProducts(data.slice(0, 12));
+        setProducts(data);
+        // setProducts(data.slice(0, 12));
       } catch (error) {
         setError("Something went wrong");
         // console.error(error);
@@ -33,15 +36,23 @@ function HomePage() {
   if (error) {
     return <h1 className="p-6 text-red-500">{error}</h1>;
   }
+  const visibleProducts=products.slice(0,visibleCount);
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold mb-6">Products</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {products.map((product: Product) => (
+        {visibleProducts.map((product: Product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+      {visibleCount<products.length&&(
+        <Pagination
+        onclick={()=>
+            setVisibleCount((prev)=>prev+Items_Per_Click)
+        }
+        />
+      )}
     </div>
   );
 }
